f71e838dcc1eebd7d3e10ac6b5d29c4d14a1c459
Adalid7 <106024914+Adalid7@users.noreply.github.com>
2022-07-10 15:50:19 -0600

    mi primer archivo java en github

---
+++ src/ejem/Ejem.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejem;
+
+/**
+ *
+ * @author loko
+ */
+public class Ejem {
+
+ 
+    public static void main(String[] args) {
+     /**
+        // ejemplo de prueva de variables int, y string
+        
+        String nombre = "adalid";
+        int followersInstagram = 2180;
+        int likeYoutube = 4356;
+        String frase = "hola mi nombre es: " + nombre + " y tengo " + (followersInstagram + likeYoutube) + " entre intagram y youtube";
+        System.out.println(frase);
+    */
+     int edad = 18;
+     
+        if (edad >=18) {
+            System.out.println("es adulto");
+            
+        } else {
+            System.out.println("es menor de edad");
+        }
+             
+     
+    } 
+    
+}
