2a0228bb7ea948a071e98602579557d7942335d6
Adalid7 <106024914+Adalid7@users.noreply.github.com>
2022-07-11 16:00:04 -0600

    mi primer archivo java en github

--- src/ejem/Ejem.java
+++ src/ejem/Ejem.java
@@ -25,7 +25,7 @@ public class Ejem {
      
         if (edad >=18) {
             System.out.println("es adulto");
-            System.out.println("creacion de una nueva rama");
+            System.out.println("prueva de github");
             
             
             
